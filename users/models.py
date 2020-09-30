df8b4b8f3ba07a6309cf573943e293bc83083d14
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-09-30 14:38:53 +0700

    finished user model fields

--- users/models.py
+++ users/models.py
@@ -5,4 +5,42 @@ from django.db import models
 
 
 class User(AbstractUser):
-    bio = models.TextField(default="")
+    """ Customer User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = [
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    ]
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_VIETNAMESE = "vn"
+    LANGUAGE_CHOICES = [
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_VIETNAMESE, "Vietnamese"),
+    ]
+
+    CURRENCY_USD = "usd"
+    CURRENCY_VND = "vnd"
+    CURRENCY_CHOICES = [
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_VND, "VND"),
+    ]
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
+    bio = models.TextField(default="", blank=True)
+    birthday = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True
+    )
+    superhost = models.BooleanField(default=False)
