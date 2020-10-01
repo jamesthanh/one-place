619100c6a43731187bd8cdfe73e426cea811b2fa
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-10-01 12:59:32 +0700

    added list display and filters on admin panel

--- users/admin.py
+++ users/admin.py
@@ -4,4 +4,5 @@ from . import models
 # Register your models here.
 @admin.register(models.User)
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("username", "email", "gender", "language", "currency", "superhost")
+    list_filter = ("superhost", "language", "currency")
